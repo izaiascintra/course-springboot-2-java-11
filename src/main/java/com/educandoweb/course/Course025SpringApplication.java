648f1e7a7e310a037fe4932990baf735d5385a4f
Izaias Cintra <izaiasacintra@gmail.com>
2020-09-03 09:13:43 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course/Course025SpringApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course025SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course025SpringApplication.class, args);
+	}
+
+}
